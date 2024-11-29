3ab5d32d40f3d3b90d97263c9ba30f77bf79626d
li <1755397894@qq.com>
2024-11-30 02:16:48 +0800

    update scrips

--- scripts/download_orangepi_mindspore_environment.sh
+++ scripts/download_orangepi_mindspore_environment.sh
@@ -2,18 +2,59 @@
 # 1.已经手动从user模式进入root用户模式，否则将会因为需要输入密码导致脚本终止
 # 2.对应的toolkit包和kernel包已经正确地安装在  /home/HwHiAiUser/Downloads  目录下
 # 3.已经配好了swap内存，否则mindspore的run_check()可能会挂掉
-cd /usr/local/Ascend/ascend-toolkit
-rm -rf *
+sudo bash -c '
 
-cd /home/HwHiAiUser/Downloads
+    set -e  # 如果任何命令失败，则立即退出脚本
 
-chmod +x ./Ascend-cann-toolkit_8.0.RC3.alpha003_linux-aarch64.run
-chmod +x ./Ascend-cann-kernels-310b_8.0.RC3.alpha003_linux-aarch64.run
-./Ascend-cann-toolkit_8.0.RC3.alpha003_linux-aarch64.run --install
-./Ascend-cann-kernels-310b_8.0.RC3.alpha003_linux-aarch64.run --install
 
-# 退出root模型，在user模式下
-exit
+
+    cd /usr/local/Ascend/ascend-toolkit || {
+
+        echo "Error: Failed to change directory to /usr/local/Ascend/ascend-toolkit. No files will be deleted." >&2
+
+        exit 1
+
+    }
+
+
+
+    rm -rf *  # 这将在cd成功后执行，如果cd失败则不会执行到这里
+
+
+
+    cd /home/HwHiAiUser/Downloads || {
+
+        echo "Error: Failed to change directory to /home/HwHiAiUser/Downloads." >&2
+
+        exit 1
+
+    }
+
+
+
+    chmod +x ./Ascend-cann-toolkit_8.0.RC3.alpha003_linux-aarch64.run
+
+    chmod +x ./Ascend-cann-kernels-310b_8.0.RC3.alpha003_linux-aarch64.run
+
+
+
+    ./Ascend-cann-toolkit_8.0.RC3.alpha003_linux-aarch64.run --install
+
+    ./Ascend-cann-kernels-310b_8.0.RC3.alpha003_linux-aarch64.run --install
+
+'
+
+
+
+# 检查sudo命令是否成功执行（即检查整个sudo bash -c命令的退出状态）
+
+if [ $? -ne 0 ]; then
+
+    echo "Error: Failed to run commands as root."
+
+    exit 1
+
+fi
 conda create --name mindspore python=3.9 -y
 conda activate mindspore
 
