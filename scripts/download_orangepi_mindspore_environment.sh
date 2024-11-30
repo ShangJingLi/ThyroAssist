6d8316e9acabeff832f708cc5c397795536dfd0a
ShangjingLee <1755397894@qq.com>
2024-11-30 19:08:37 +0800

    modify scripts

--- scripts/download_orangepi_mindspore_environment.sh
+++ scripts/download_orangepi_mindspore_environment.sh
@@ -30,93 +30,22 @@ pip config set global.index-url https://pypi.tuna.tsinghua.edu.cn/simple
 pip install openi
 cd /home/HwHiAiUser/Downloads || exit
 
-# 定义要查找的包模式
-kernel_pattern="Ascend-cann-kernels-*.run"
-toolkit_pattern="Ascend-cann-toolkit_*.run"
+python -c "import os; import fnmatch; import openi;prefix_toolkit='Ascend-cann-toolkit'; \
+prefix_kernels='Ascend-cann-kernels'; extension='.run'; \
+found_toolkit = any(f.startswith(prefix_toolkit) and f.endswith(extension) for f in os.listdir('.')); \
+found_kernels = any(f.startswith(prefix_kernels) and f.endswith(extension) for f in os.listdir('.')); \
+if not found_toolkit: openi.download_model(repo_id='enter/nodule_segmentation', model_name='cann-toolkit', save_path='.') \
+if not found_kernels: openi.download_model(repo_id='enter/nodule_segmentation', model_name='cann-kernels', save_path='.') \
+toolkit_path = next((f for f in os.listdir(os.getcwd()) if fnmatch.fnmatch(f, f'{prefix_toolkit}*')), None); \
+kernels_path = next((f for f in os.listdir(os.getcwd()) if fnmatch.fnmatch(f, f'{prefix_kernels}*')), None);" || exit
 
-# 当前目录
-current_directory=$(pwd)
+export TOOLKIT_NAME=$(python -c "import os;import fnmatch;prefix_toolkit='Ascend-cann-toolkit'; extension='.run';found_toolkit = any(f.startswith(prefix_toolkit) and f.endswith(extension) for f in os.listdir('.')); toolkit_path = next((f for f in os.listdir(os.getcwd()) if fnmatch.fnmatch(f, 'Ascend-cann-toolkit*')), None); print(toolkit_path)")
 
-# 查找当前目录中的kernel包
-kernel_files=$(find "$current_directory" -maxdepth 1 -type f -name "$kernel_pattern")
+export KERNELS_NAME=$(python -c "import os;import fnmatch;prefix_kernels='Ascend-cann-kernels';extension='.run';found_kernels = any(f.startswith(prefix_kernels) and f.endswith(extension) for f in os.listdir('.'));kernels_path = next((f for f in os.listdir(os.getcwd()) if fnmatch.fnmatch(f, 'Ascend-cann-kernels*')), None);print( kernels_path);")
 
-# 查找当前目录中的toolkit包
-toolkit_files=$(find "$current_directory" -maxdepth 1 -type f -name "$toolkit_pattern")
 
-# 检查toolkit包是否存在
-if [ -z "$toolkit_files" ]; then
-    echo "No toolkit package found. Starting download..."
-    openi model download enter/nodule_segmentation cann-toolkit --save_path .
 
-    # 检查下载是否成功（假设下载后的文件名与模式匹配）
-    downloaded_toolkit_files=$(find "$current_directory" -maxdepth 1 -type f -name "$toolkit_pattern")
-    if [ -z "$downloaded_toolkit_files" ]; then
-        echo "Failed to download toolkit package."
-        exit 1
-    else
-        echo "Toolkit package downloaded successfully: $downloaded_toolkit_files"
-    fi
-fi
-
-# 检查kernel包是否存在
-if [ -z "$kernel_files" ]; then
-    echo "No kernel package found. Starting download..."
-    openi model download enter/nodule_segmentation cann-kernels --save_path .
-
-    # 检查下载是否成功（假设下载后的文件名与模式匹配）
-    downloaded_kernel_files=$(find "$current_directory" -maxdepth 1 -type f -name "$kernel_pattern")
-    if [ -z "$downloaded_kernel_files" ]; then
-        echo "Failed to download kernel package."
-        exit 1
-    else
-        echo "Kernel package downloaded successfully: $downloaded_kernel_files"
-    fi
-fi
-pip uninstall openi -y
-
-sudo bash -c '
-
-    set -e  # 如果任何命令失败，则立即退出脚本
-
-    CANN_DIR="."
-
-    # 查找toolkit和kernels的安装包
-    TOOLKIT_FILE=$(ls "$CANN_DIR"/Ascend-cann-toolkit_*.run 2>/dev/null | sort -V | tail -n 1)
-    KERNELS_FILE=$(ls "$CANN_DIR"/Ascend-cann-kernels_*.run 2>/dev/null | sort -V | tail -n 1)
-
-    # 检查是否找到了安装包
-    if [ -z "$TOOLKIT_FILE" ]; then
-      echo "Error: No CANN toolkit package found in $CANN_DIR"
-      exit 1
-    fi
-
-    if [ -z "$KERNELS_FILE" ]; then
-      echo "Error: No CANN kernels package found in $CANN_DIR"
-      exit 1
-    fi
-
-    # 赋予安装包执行权限
-    chmod +x "$TOOLKIT_FILE"
-    chmod +x "$KERNELS_FILE"
-
-    # 安装toolkit
-    echo "Installing CANN toolkit..."
-    sudo "$TOOLKIT_FILE" --install -y
-    if [ $? -ne 0 ]; then
-      echo "Error: Failed to install CANN toolkit"
-      exit 1
-    fi
-
-    # 安装kernels
-    echo "Installing CANN kernels..."
-    sudo "$KERNELS_FILE" --install -y
-    if [ $? -ne 0 ]; then
-      echo "Error: Failed to install CANN kernels"
-      exit 1
-    fi
-
-    echo "CANN toolkit and kernels have been installed successfully."
-'
+sudo -E bash -c "\"chmod +x ./${TOOLKIT_NAME} && chmod +x ./${KERNELS_NAME} && ./${TOOLKIT_NAME} --install -y && ./${KERNELS_NAME} --install -y\"" || exit
 
 # 安装mindspore部分
 python -m pip install -U pip
--- scripts/train_and_save_checkpoints.sh
+++ scripts/train_and_save_checkpoints.sh
@@ -8,8 +8,3 @@ python train_and_eval/segmentation/train_unet.py
 python -c "import os; from src.deep_learning.utils import split_ckpt; \
 file_path = os.path.join('segmentation_checkpoints', 'unet_checkpoints.ckpt'); \
 split_ckpt(file_path, chunk_size=80 * 1024 * 1024)"
-
-
-
-
-
