0b6ce35680469afc527c13f3959e05ee538e661d
ShangjingLee <1755397894@qq.com>
2024-12-08 10:54:52 +0800

    modify pylint

--- scripts/pylint_check.sh
+++ scripts/pylint_check.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 # pylint检查脚本，用于进行代码风格检查
 # Windows系统下无法直接运行本脚本，请依次输入如下命令进行代码风格检查：
-# pylint --rcfile=.gitee\.pylint.conf src
-# pylint --rcfile=.gitee\.pylint.conf demo
-# pylint --rcfile=.gitee\.pylint.conf train_and_eval
+# pylint --rcfile=.pylint.conf src
+# pylint --rcfile=.pylint.conf demo
+# pylint --rcfile=.pylint.conf train_and_eval
 NUM_CORES=$(nproc)
-pylint --jobs=$NUM_CORES --rcfile=.gitee/.pylint.conf src
-pylint --jobs=$NUM_CORES --rcfile=.gitee/.pylint.conf demo
-pylint --jobs=$NUM_CORES --rcfile=.gitee/.pylint.conf train_and_eval
+pylint --jobs=$NUM_CORES --rcfile=.pylint.conf src
+pylint --jobs=$NUM_CORES --rcfile=.pylint.conf demo
+pylint --jobs=$NUM_CORES --rcfile=.pylint.conf train_and_eval
