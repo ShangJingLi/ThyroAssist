3667f2a033ca21ab56c3f7d7a76ec84ab883d37e
李尚憬 <1755397894@qq.com>
2025-03-05 03:37:11 +0000

    修改香橙派自动安装脚本的mindspore版本
    Signed-off-by: 李尚憬 <1755397894@qq.com>

--- scripts/orangepi/mindspore_installer.sh
+++ scripts/orangepi/mindspore_installer.sh
@@ -7,11 +7,11 @@ pip install sympy
 pip install /usr/local/Ascend/ascend-toolkit/latest/lib64/te-*-py3-none-any.whl
 pip install /usr/local/Ascend/ascend-toolkit/latest/lib64/hccl-*-py3-none-any.whl
 
-export MS_VERSION=2.4.1
+export MS_VERSION=2.5.0
 pip install \
-    https://ms-release.obs.cn-north-4.myhuaweicloud.com/${MS_VERSION}/MindSpore/unified/aarch64/mindspore-${MS_VERSION/-/}-cp39-cp39-linux_aarch64.whl \
-    --trusted-host ms-release.obs.cn-north-4.myhuaweicloud.com \
-    -i https://pypi.tuna.tsinghua.edu.cn/simple
+https://ms-release.obs.cn-north-4.myhuaweicloud.com/${MS_VERSION}/MindSpore/unified/aarch64/mindspore-${MS_VERSION/-/}-cp311-cp311-linux_aarch64.whl \
+--trusted-host ms-release.obs.cn-north-4.myhuaweicloud.com \
+-i https://pypi.tuna.tsinghua.edu.cn/simple
 pip install jinja2 absl-py
 
 
