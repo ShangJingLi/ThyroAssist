f94594571d2fd3bdf25ee1cc549df292214be0c3
ShangjingLee <1755397894@qq.com>
2025-03-06 19:15:37 +0800

    完善resnet部分数据处理模块

--- src/machine_learning/dataloader/load_resnet_data.py
+++ src/machine_learning/dataloader/load_resnet_data.py
@@ -1,4 +1,6 @@
 import os
+import warnings
+import random
 import multiprocess
 import cv2
 import numpy as np
@@ -6,63 +8,148 @@ import mindspore.dataset as ds
 from mindspore.dataset.vision.transforms import Rescale, HWC2CHW
 
 
-def boundary_padding(origin_images:np.array, aim_size:tuple, padding:int):
+def boundary_padding(origin_images:np.array, padding, aim_size:tuple=(572, 572)):
+    if len(aim_size) != 2:
+        raise ValueError(f"The length of 'aim_size' must be 2, but got {len(aim_size)}!")
+
     if origin_images.ndim == 4:
         processed_images = np.ones(shape=(origin_images.shape[0], aim_size[0], aim_size[1], 3),
                                    dtype=np.uint8) * padding
-        k = origin_images.shape[1] / origin_images.shape[2]  # k = 原始高 / 原始宽 < 1
-        for i in range(origin_images.shape[0]):
-            image = cv2.resize(origin_images[i], dsize=(aim_size[1], int(k * aim_size[1])))
-            dist = (image.shape[2] - image.shape[1]) // 2
-            processed_images[i][dist:dist+image.shape[0], :, :] = image
+        k = origin_images.shape[1] / origin_images.shape[2]  # k = 原始高 / 原始宽
+        if k < 1:
+            for i in range(origin_images.shape[0]):
+                image = cv2.resize(origin_images[i], dsize=(aim_size[1], int(k * aim_size[1])))
+                dist = (image.shape[1] - image.shape[0]) // 2
+                processed_images[i][dist:dist+image.shape[0], :, :] = image
+        else:
+            for i in range(origin_images.shape[0]):
+                image = cv2.resize(origin_images[i], dsize=(int(aim_size[1] / k), aim_size[1]))
+                dist = (image.shape[0] - image.shape[1]) // 2
+                processed_images[i][ :,dist:dist+image.shape[1], :] = image
 
         return processed_images
 
     elif origin_images.ndim == 3:
-        k = origin_images.shape[0] / origin_images.shape[1]  # k = 原始高 / 原始宽 < 1
-        image = cv2.resize(origin_images, dsize=(aim_size[1], int(k * aim_size[1])))
-        processed_image = np.ones(shape=(aim_size[0], aim_size[1], 3), dtype=np.uint8) * padding
-        dist = (image.shape[1] - image.shape[0]) // 2
-        processed_image[dist:dist+image.shape[0], :, :] = image
+        k = origin_images.shape[0] / origin_images.shape[1]  # k = 原始高 / 原始宽
+        if k < 1:
+            image = cv2.resize(origin_images, dsize=(aim_size[1], int(k * aim_size[1])))
+            processed_image = np.ones(shape=(aim_size[0], aim_size[1], 3), dtype=np.uint8) * padding
+            dist = (image.shape[1] - image.shape[0]) // 2
+            processed_image[dist:dist+image.shape[0], :, :] = image
+        else:
+            image = cv2.resize(origin_images, dsize=(int(aim_size[1] / k), aim_size[1]))
+            processed_image = np.ones(shape=(aim_size[0], aim_size[1], 3), dtype=np.uint8) * padding
+            dist = (image.shape[0] - image.shape[1]) // 2
+            processed_image[ :,dist:dist+image.shape[1], :] = image
+        return processed_image
+
+    else:
+        raise ValueError(f"'origin_images.ndim' must be 3 or 4, but got {origin_images.ndim}!")
+
+
+def center_crop(origin_images:np.array, aim_size:tuple=(572, 572)):
+    """保留原图像最中心的正方形部分，并将其像素值置为aim_size"""
+    if len(aim_size) != 2:
+        raise ValueError(f"The length of 'aim_size' must be 2, but got {len(aim_size)}!")
+    if origin_images.ndim == 4:
+        processed_images = np.ones(shape=(origin_images.shape[0], aim_size[0], aim_size[1], 3), dtype=np.uint8)
+        k = origin_images.shape[2] / origin_images.shape[1]
+        if k > 1:
+            for i in range(origin_images.shape[0]):
+                image = cv2.resize(origin_images[i], dsize=(int(k * aim_size[0]), aim_size[0]))
+                processed_images[i] = image[:, int((k - 1) * aim_size[0]): int((k - 1) * aim_size[0]) + aim_size[0], :]
+        else:
+            for i in range(origin_images.shape[0]):
+                image = cv2.resize(origin_images[i], dsize=(aim_size[1], int(aim_size[1] / k)))
+                processed_images[i] = image[int((1 - k) * aim_size[1]): int((1 - k) * aim_size[1]) + aim_size[1], :, :]
+
+        return processed_images
+
+    elif origin_images.ndim == 3:
+        k = origin_images.shape[1] / origin_images.shape[0]
+        if k > 1:
+            image = cv2.resize(origin_images, dsize=(int(k * aim_size[0]), aim_size[0]))
+            processed_image = image[:, (k - 1) * aim_size[0]: (k - 1) * aim_size[0] + aim_size[0], :]
+        else:
+            image = cv2.resize(origin_images, dsize=(aim_size[1], int(aim_size[1] / k)))
+            processed_image = image[int((1 - k) * aim_size[1]): int((1 - k) * aim_size[1]) + aim_size[1], :]
 
         return processed_image
 
+    else:
+        raise ValueError(f"'origin_images.ndim' must be 3 or 4, but got {origin_images.ndim}!")
 
-def convert_to_numpy(images_path):
+
+def convert_to_numpy(images_path, method:str, padding:int = None, aim_size:tuple=(572, 572)):
+    """将.jpg格式图片转存为numpy数组，便于后续处理"""
+    if method not in ["pad", "crop"]:
+        raise ValueError(f"Invalid method '{method}'. Valid methods are 'pad' and 'crop'.")
+
+    if method == "pad" and padding is None:
+        raise ValueError("When method is 'pad', 'padding' must be provided.")
+
+    if method == "crop" and padding is not None:  # 检查padding是否被显式设置（且不等于默认值255）
+        warnings.warn("When method is 'crop', 'padding' parameter is ignored and will have no effect.", UserWarning)
+
+
     # 过滤出所有的jpg文件（忽略大小写）
     class_a_files = [file for file in os.listdir(os.path.join(images_path, "A")) if file.lower().endswith('.jpg')]
     class_b_files = [file for file in os.listdir(os.path.join(images_path, "B")) if file.lower().endswith('.jpg')]
 
-    images_a = np.zeros(shape=(len(class_a_files), 572, 572, 3), dtype=np.uint8)
-    images_b = np.zeros(shape=(len(class_b_files), 572, 572, 3), dtype=np.uint8)
+    random.shuffle(class_a_files)
+    random.shuffle(class_b_files)
 
-    flag = 0
+    images_a = np.zeros(shape=(len(class_a_files), aim_size[0], aim_size[1], 3), dtype=np.uint8)
+    images_b = np.zeros(shape=(len(class_b_files), aim_size[0], aim_size[1], 3), dtype=np.uint8)
 
     for i in range(len(class_a_files)):
-        image = cv2.imread(os.path.join(images_path, "A", f"A{i+1}.jpg"))
+        image = cv2.imread(os.path.join(images_path, "A", f"{class_a_files[i]}"))
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        image = boundary_padding(image, (572, 572), padding=255)
+        if method == "pad":
+            image = boundary_padding(image, aim_size=aim_size, padding=padding)
+        else:
+            image = center_crop(image, aim_size)
         images_a[i] = image
 
     for i in range(len(class_b_files)):
-        flag += 1
-        image = cv2.imread(os.path.join(images_path, "B", f"B{i+1}.jpg"))
+        image = cv2.imread(os.path.join(images_path, "B", f"{class_b_files[i]}"))
         image = cv2.cvtColor(image, cv2.COLOR_BGR2RGB)
-        image = boundary_padding(image, (572, 572), padding=255)
+        if method == "pad":
+            image = boundary_padding(image, aim_size=aim_size, padding=padding)
+        else:
+            image = center_crop(image, aim_size)
         images_b[i] = image
 
     return images_a, images_b
 
 
-def generate_images_and_labels(images_a:np.array, images_b:np.array):
-    train_images = np.concatenate((images_a[:-50, :, :, :],
-                                   images_b[:-50, :, :, :]), axis=0)
-    train_labels = np.concatenate((np.tile(np.array([1, 0]), reps=(images_a.shape[0] - 50, 1)),
-                                   np.tile(np.array([0, 1]), reps=(images_b.shape[0] - 50, 1))))
-    val_images = np.concatenate((images_a[-50:, :, :, :],
-                                   images_b[-50:, :, :, :]), axis=0)
-    val_labels = np.concatenate((np.tile(np.array([1, 0]), reps=(50, 1)),
-                                   np.tile(np.array([0, 1]), reps=(50, 1))))
+def generate_images_and_labels(images_a:np.array, images_b:np.array, batch_size=16):
+    """传入以numpy数组存取的A类和B类图像，划分训练集和测试集,数据集大小至少要大于2 * batch_size"""
+
+    # 确定预测集数，确保训练集数为16的倍数，避免训练过程中因凑不满一个batch导致数据浪费
+    n_images =  images_a.shape[0] + images_b.shape[0]
+    if n_images < 100:
+        warnings.warn("The dataset is too small!", UserWarning)
+    n = int(0.95 * n_images)
+    q = n // batch_size
+    r = n % batch_size  # 余数
+    if r > (0.5 * batch_size):
+        n_train = batch_size * (q + 1)
+    else:
+        n_train = batch_size * q
+
+    n_val = n_images - n_train
+    n_val_a = int(0.5 * n_val)
+    n_val_b = n_val - n_val_a
+
+    train_images = np.concatenate((images_a[:-n_val_a, :, :, :],
+                                   images_b[:-n_val_b, :, :, :]), axis=0)
+    train_labels = np.concatenate((np.tile(np.array([1, 0]), reps=(images_a.shape[0] - n_val_a, 1)),
+                                   np.tile(np.array([0, 1]), reps=(images_b.shape[0] - n_val_b, 1))))
+    val_images = np.concatenate((images_a[-n_val_a:, :, :, :],
+                                   images_b[-n_val_b:, :, :, :]), axis=0)
+    val_labels = np.concatenate((np.tile(np.array([1, 0]), reps=(n_val_a, 1)),
+                                   np.tile(np.array([0, 1]), reps=(n_val_b, 1))))
 
     return train_images, train_labels, val_images, val_labels
 
@@ -119,4 +206,5 @@ def create_dataset_with_numpy(images, labels, batch_size, is_train):
 __all__ = ["create_dataset_with_numpy",
            "generate_images_and_labels",
            "boundary_padding",
-           "convert_to_numpy"]
+           "convert_to_numpy",
+           "center_crop"]
